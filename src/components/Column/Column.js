867b9021b79a762db2f9fab5dbe11bb13928db3a
Piotr Wilczynski <tancerz@autograf.pl>
2020-02-11 19:06:43 +0100

    Adding column and card by creator

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -11,6 +11,7 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.node.isRequired,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
@@ -19,7 +20,7 @@ class Column extends React.Component {
 
   render() {
 
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
 
     return (
       <section className={styles.component}>
@@ -31,11 +32,11 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-{/*
+
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
         </div>
-*/}
+
       </section>
 
     );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ import Hero from '../Hero/Hero.js';
 import Column from '../Column/ColumnContainer.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
 
@@ -13,6 +13,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string.isRequired,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -21,7 +22,7 @@ class List extends React.Component {
 
   render() {
 
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
 
     return (
       <section className={styles.component}>
@@ -36,12 +37,12 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/*
+
 
         <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
         </div>
-*/}
+
       </section>
     );
   }
