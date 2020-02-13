23c08b809878a7644651963be1481f2860fc58c5
Piotr Wilczynski <tancerz@autograf.pl>
2020-02-13 08:47:32 +0100

    Add div Container (wrapper for List and Search)

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import Column from '../Column/ColumnContainer.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 import Creator from '../Creator/Creator.js';
+import Container from '../Container/Container.js'
 
 class List extends React.Component {
 
@@ -26,23 +27,24 @@ class List extends React.Component {
 
     return (
       <section className={styles.component}>
-        <Hero titleText={title} image={image}/>
+        <Container>
+          <Hero titleText={title} image={image}/>
 
-        <div className={styles.description}>
-          ReactHtmlParser({description})
-        </div>
+          <div className={styles.description}>
+            ReactHtmlParser({description})
+          </div>
 
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
 
 
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
-
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+        </Container>
       </section>
     );
   }
