34f88140057020d02ccba082745ac9c68e581f49
Piotr Wilczynski <tancerz@autograf.pl>
2020-02-13 09:10:24 +0100

    Add div Container (wrapper for List and Search)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import styles from './App.scss';
-import List from '../List/ListContainer.js';
-import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer.js'
+import Home from '../Home/HomeContainer';
 
-class App extends React.Component {
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array,
-  }
-
-  render() {
-    const {title, subtitle, lists} = this.props;
-    return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-      </main>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Home />
+  </div>
+);
 
 export default App;
